86bc62a9adeab85e7dc537d18ee7b6c0fb280eba
Alexandra Di Giulian <andigiulian@gmail.com>
2017-06-08 17:31:39 -0500

    Finished exercise

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,36 @@
-#write your code here
+def add (x, y)
+	x + y
+end
+
+def subtract (x, y)
+	x - y
+end
+
+def sum (array)
+	total = 0
+	array.each do |num|
+		total += num
+	end	 
+	total
+end
+
+def multiply (*num)
+	total = 1
+	num.each do |int|
+		total *= int
+	end
+	total
+end
+
+def power (x, y)
+	x ** y
+end
+
+def factorial (num)
+	sum = 1
+	while (num > 1)
+		sum *= num
+		num -= 1
+	end
+	sum
+end
